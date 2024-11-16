ac0f502331845d7ed8d53cdf10243715899bd9ab
Diego Vázquez <99550587+dvrvk@users.noreply.github.com>
2024-11-16 19:56:40 +0100

    feat: nuevo endpoint para obtener todas las imagenes del producto por id en base64

--- src/main/java/com/mercaline/controller/ImageController.java
+++ src/main/java/com/mercaline/controller/ImageController.java
@@ -1,27 +1,25 @@
 package com.mercaline.controller;
 
-import com.mercaline.dto.ProductResponseSummaryDTO;
+import com.mercaline.dto.ApiResponse;
 import com.mercaline.error.exceptions.ImageNotFound;
 import com.mercaline.error.exceptions.ProductoNotFoundException;
 import com.mercaline.model.ProductEntity;
 import com.mercaline.service.ProductService;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 import java.util.regex.Pattern;
 
 @RestController
@@ -38,7 +36,7 @@ public class ImageController {
         ProductEntity product = this.productService.findById(id).orElseThrow(ProductoNotFoundException::new);
         String[] imagesUrls = product.getUrlImage().split(";");
         // Para evitar que la imagenes de prueba fallen
-        if(!isUrl(imagesUrls[0])) {
+        if(!isURL(imagesUrls[0])) {
             try {
                 Path path = Paths.get(imagesUrls[0]);
                 byte[] imageBytes = Files.readAllBytes(path);
@@ -63,8 +61,42 @@ public class ImageController {
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findAll(@PathVariable Long id) {
 
-    private boolean isUrl(String path) {
+        ProductEntity product = this.productService.findById(id)
+                .orElseThrow(ProductoNotFoundException::new);
+
+        String[] imageResources = product.getUrlImage().split(";");
+
+        // Evita que las imagenes de prueba fallen
+        if(!isURL(imageResources[0])) {
+            List<String> base64Images = new ArrayList<>();
+
+            for(String imageResource : imageResources) {
+                try {
+                    Path path = Paths.get(imageResource);
+                    byte[] imageBytes = Files.readAllBytes(path);
+                    String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+                    base64Images.add(base64Image);
+                } catch (IOException e) {
+                    String imageName = Paths.get(imageResources[0]).getFileName().toString();
+                    throw new ImageNotFound(imageName);
+                }
+            }
+            // Convierte la lista de imágenes Base64 en una cadena separada por comas
+            String imagesString = String.join(",", base64Images);
+            ApiResponse response = new ApiResponse(HttpStatus.OK, imagesString);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+
+        return ResponseEntity.ok(imageResources[0]);
+
+
+    }
+
+
+    private boolean isURL(String path) {
         String urlPattern = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$";
         return Pattern.matches(urlPattern, path);
     }
